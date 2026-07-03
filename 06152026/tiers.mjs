d6b432160912b77d4942d3d5a6c492011bc1e35c
cdebnath9498 <coolsayan9498@gmail.com>
2026-07-02 21:08:08 -0700

    Update tiers.mjs

--- 06152026/tiers.mjs
+++ 06152026/tiers.mjs
@@ -4,15 +4,7 @@
     { name: "enterprise", limit: Infinity },
   ];
 
-  function pickTier(usage) {
+  export function pickTier(usage) {
     const tier = TIERS.find((t) => usage > t.limit);
     return tier.name;
   }
-
-  function describe(account) {
-    return `${account.org} is on the ${pickTier(account.monthlyUsage)} tier`;
-  }
-
-  setTimeout(() => {
-    console.log(describe({ org: "Acme", monthlyUsage: 50 }));
-  }, 400);
